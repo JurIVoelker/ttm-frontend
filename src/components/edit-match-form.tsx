625a201e40ac1dbcb5d7847d790b3c2819c3c1d0
JuriVoelker <mail@jurivoelker.de>
2026-01-23 18:04:46 +0100

    fix: adjust className for EditMatchForm component to improve layout

--- src/components/edit-match-form.tsx
+++ src/components/edit-match-form.tsx
@@ -151,6 +151,7 @@ const EditMatchForm: React.FC<EditMatchFormProps> = ({
           onSave={() => {}}
           isSaving={isLoading}
           backNavigation={`/${teamSlug}#match-card-${match?.id}`}
+          className="-top-16"
         />
         <div className="space-y-6">
           {isCreate && (
